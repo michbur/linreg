886ff9480e2fb689a7be83f69174ff13942a7ba9
michbur <michalburdukiewicz@gmail.com>
2015-05-04 14:37:08 +0200

    aded linreg diagram

--- tests.R
+++ tests.R
@@ -7,3 +7,12 @@ baseline(fluo = reps[, 28])
 #eff 1,831
 #R^2 0.99999
 #cycles 11-15 - linear
+
+
+#look for jumps
+lapply(2L:ncol(reps), function(i) {
+  ders <- summary(inder(reps[, 1], reps[, i], smooth.method = NULL),  print = FALSE)
+  exp_end <- round(ders[["SDM"]], 0)
+  
+  sapply(exp_end:2, function(fluo_id) (reps[fluo_id, i] - reps[fluo_id - 1, i])) 
+})
