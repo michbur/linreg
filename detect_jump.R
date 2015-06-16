e0f817a8fa4eb1d56581ac33f7229e81442c7e64
michbur <michalburdukiewicz@gmail.com>
2015-06-16 22:26:01 +0200

    added discovery - no baselining in hard situations

--- detect_jump.R
+++ detect_jump.R
@@ -2,13 +2,18 @@ library(reshape2)
 library(ggplot2)
 library(dplyr)
 
+library(qpcR)
 
 cyc <- reps[, 1]
 y <- reps[, 11]
 
+
+#The start of the exponential phase is defined by a jump: when fluorescence in cycle C+1 is less 
+#than the fluorescence in cycle C then the exponential phase starts at cycle c
 bot <- c(NA, sapply(2L:length(y), function(i) y[i] - y[i - 1])) < 0
 top <- c(NA, sapply(2L:length(y), function(i) y[i - 1] - y[i])) < 0
-
+#Linreg: all samples excluded
+#does linreg estimate eff without baselining?
 
 data.frame(cyc, y, bot, top) %>% melt(id.vars = "cyc") %>%
   ggplot(aes(x = cyc, y = value, colour = variable)) +
