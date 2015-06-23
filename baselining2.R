2fdd2ae6bf78a389ca6276718dca59a7b08a18b9
michbur <michalburdukiewicz@gmail.com>
2015-06-23 11:58:20 +0200

    did I finish baselining?

--- baselining2.R
+++ baselining2.R
@@ -19,7 +19,7 @@ get_exp <- function(y) {
 
 #get single slope
 get_slope <- function(y)
-  coef(lm(y ~ x, data = data.frame(x = (1L:length(y)), y = y)))[["x"]]
+  coef(lm(y ~ x, data = data.frame(x = (1L:length(y)), y = log(y))))[["x"]]
 
 #get both slopes
 get_slopes <- function(y) {
@@ -27,6 +27,8 @@ get_slopes <- function(y) {
   #exponential phase
   expp <- y[exp_borders["start"]:exp_borders["end"]]
   lexpp <- length(expp)
+  
+  #range of regions
   if(lexpp %% 2 == 0) {
     #exponential phase bottom
     exppl <- expp[1L:(lexpp/2)]
@@ -38,88 +40,92 @@ get_slopes <- function(y) {
     #exponential phase upper
     exppu <- expp[(lexpp/2):lexpp]
   }
-  c(l = get_slope(exppl), u = get_slope(exppu))
+  
+  c(l = get_slope(abs(exppl)), u = get_slope(abs(exppu)))
 }
 
 
 library(qpcR)
-fluo <- rutledge[, 2]
-cyc <- rutledge[, 1]
-max.it <- 10000
 
 # baselining function --------------------------------------------
 
-
-#if sample is not amplified, end function
-is_amplified(fluo)
-
-
-#preserve raw input
-raw_fluo <- fluo
-
-bl <- min(fluo)
-fluo <- fluo - bl
-#if min(data) is 0, we need to add small epsilon, because linreg 
-#often uses proportion of minimum and maximum value
-if(bl == 0) {
-  fluo <- fluo + 0.01*max(fluo)
-  bl <- min(fluo)
-}
-
-
-#Part 1 ---------------------------
-
-
-#new baseline: mean of 6th and 7th point before the plateau phase
-#bl <- mean(fluo[(which.min(simple_derivation(simple_derivation(fluo)))-7:6)])
-#fluo <- fluo - bl
-
-sl <- get_slopes(fluo)
-
-it <- 0
-while(sl["u"] <  sl["l"]  && it < max.it) {
-  bl <- bl * 0.99
+baseline <- function(fluo, max_it = 50) {
   
-  #add here if statement
-  bl < min(fluo)
-
+  #if sample is not amplified, end function
+  is_amplified(fluo)
+  
+  
+  #preserve raw input
+  raw_fluo <- fluo
+  
+  bl <- min(fluo)
+  fluo <- fluo - bl
+  #if min(data) is 0, we need to add small epsilon, because linreg 
+  #often uses proportion of minimum and maximum value
+  if(bl == 0) {
+    fluo <- fluo + 0.001*max(fluo)
+    bl <- min(fluo)
+  }
+  
+  
+  #Part 1 ---------------------------
+  
+  
+  #new baseline: mean of 6th and 7th point before the plateau phase
+  bl <- mean(fluo[(which.min(simple_derivation(simple_derivation(fluo)))-2:1)])
+  fluo <- fluo - bl
+  sl <- get_slopes(fluo)
+  
+  it <- 0
+  while(sl["u"] <  sl["l"]  && it < max_it) {
+    bl <- bl * 0.99
     
+    #add here if statement
+    bl < min(fluo)
+    
+    
+    fluo <- fluo - bl
+    
+    sl <- get_slopes(fluo)
+    it <- it + 1
+  }
+  
+  stp <- 0.005*bl 
+  
+  # Part 2 ---------------------------------
+  
+  bl <- bl + stp
   fluo <- fluo - bl
   
   sl <- get_slopes(fluo)
-  it <- it + 1
-}
-
-stp <- 0.05*bl 
-
-# Part 2 ---------------------------------
-
-bl <- bl + stp
-fluo <- fluo - bl
-
-max.it = 10
-
-it <- 0
-while(abs(sl["u"] -  sl["l"]) > 1e-5  && it < max.it) {
-  if(sl["u"] < sl["l"]) {
-    bl <- bl - 2*stp
-    stp <- stp/2
+  
+  max_it = 50
+  
+  it <- 0
+  while(abs(sl["u"] -  sl["l"]) > 1e-5  && it < max_it) {
+    if(sl["u"] < sl["l"]) {
+      bl <- bl - 2*stp
+      stp <- stp/2
+      
+      fluo <- fluo - bl
+      
+      sl <- get_slopes(fluo)
+      
+      it <- it + 1
+      
+    } else {
+      bl <- bl + stp
+      fluo <- fluo - bl
+      
+      sl <- get_slopes(fluo)
+      
+      it <- it + 1
+    }
     
-    fluo <- fluo - bl
-    
-    sl <- get_slopes(fluo)
-    
-    it <- it + 1
-    
-  } else {
-    bl <- bl + stp
-    fluo <- fluo - bl
-    
-    sl <- get_slopes(fluo)
-    
-    it <- it + 1
-    
-    print(abs(sl["u"] -  sl["l"]))
-
   }
+  
+  bl
 }
+
+
+baseline(rutledge[, 2])
